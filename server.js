54094297146d3bcab9f84308ba1e794994381843
SmallPeenwow <bowdencamryn@gmail.com>
2022-08-23 21:04:32 +0200

    User join chat fine tuned

--- server.js
+++ server.js
@@ -17,16 +17,22 @@ const botName = 'ChatCord Bot';
 // Run when client connects
 io.on('connection', (socket) => {
 	socket.on('joinRoom', ({ username, room }) => {
+		const user = userJoin(socket.id, username, room);
+
+		socket.join(user.room);
+
 		// Welcome current user
 		socket.emit('message', formatMessage(botName, 'Welcome to ChatApp'));
 
 		// Broadcast when a user connects
-		socket.broadcast.emit('message', formatMessage(botName, 'A user has joined the chat'));
+		socket.broadcast.to(user.room).emit('message', formatMessage(botName, `${user.username} has joined the chat`));
 	});
 
 	// Listen for chatMessage
 	socket.on('chatMessage', (msg) => {
-		io.emit('message', formatMessage('USER', msg));
+		const user = getCurrentUser(socket.id);
+
+		io.to(user.room).emit('message', formatMessage(user.username, msg));
 	});
 
 	// Runs when client disconnects
