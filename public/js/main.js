808bd4b9588e52ff24e21f2afd4e5c3347f856c1
SmallPeenwow <bowdencamryn@gmail.com>
2022-08-23 17:55:36 +0200

    Message send and receive done

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,40 @@
+const chatForm = document.getElementById('chat-form');
+const chatMessage = document.querySelector('.chat-message');
+
 const socket = io();
 
-socket.on('message', (message) => {});
+// Message from server
+socket.on('message', (message) => {
+	outputMessage(message);
+
+	// Scroll down
+	chatMessage.scrollTop = chatMessage.scrollHeight;
+});
+
+// Message submit
+chatForm.addEventListener('submit', (event) => {
+	event.preventDefault();
+
+	// Get message text
+	const msg = event.target.elements.msg.value;
+
+	// Emit message to server
+	socket.emit('chatMessage', msg);
+
+	// Clear input
+	event.target.elements.msg.value = '';
+	event.target.elements.msg.focus();
+});
+
+// Output message to DOM
+function outputMessage(message) {
+	const div = document.createElement('div');
+	div.classList.add('message');
+
+	div.innerHTML = `<p class="meta">Brad <span>9:12pm</span></p>
+    <p class="text">
+        ${message}
+    </p>`;
+
+	document.querySelector('.chat-messages').appendChild(div);
+}
